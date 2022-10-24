d3d3b6a2179effa2c44bf741f41a61c5dd4e9077
Spencer Buehlman <sbuehlman864@gmail.com>
2022-10-24 15:22:38 -0400

    Made projectile abstract
    made projectile move method abstract

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,8 @@ import scenes.GalagaScene;
 public class Main {
 
   public static void main (String[] args) {
-	  //Application.launch(BreakoutScene.class, args);
-	  Application.launch(GalagaScene.class, args);
+	  Application.launch(BreakoutScene.class, args);
+	  //Application.launch(GalagaScene.class, args);
   }
     
 }
--- src/functionality/HighScore.java
+++ src/functionality/HighScore.java
@@ -77,6 +77,7 @@ public class HighScore {
 			exp.printStackTrace();
 		} 
 		return currentHighScore;
-
 	}
+	
+	
 }
